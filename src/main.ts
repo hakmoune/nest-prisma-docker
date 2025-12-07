e8e9ab589e7eb9c6369dfd752151858e5ad24f28
El MAHDI HAKMOUNE <heymehdi@MacBook-Pro-de-El.local>
2025-12-07 16:42:49 +0100

    feat: add global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //app.setGlobalPrefix('api/v1'); // All routes will be prefixed with /api/v1/products/id
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Ignore les propriétés non définies dans les DTO
