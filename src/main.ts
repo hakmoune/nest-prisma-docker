efec83ed4c38c0e9cf6b7ad8633de4f848d59edb
El MAHDI HAKMOUNE <heymehdi@MacBook-Pro-de-El.local>
2025-12-07 21:58:09 +0100

    feat: validationPipe add comment to explain the usage

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   //app.setGlobalPrefix('api/v1'); // All routes will be prefixed with /api/v1/products/id
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Ignore les propriétés non définies dans les DTO
-      forbidNonWhitelisted: true, // Lève une erreur si des propriétés non définies sont présentes
+      whitelist: true, // Ignore les propriétés non définies dans les DTO, Supprime automatiquement le champ "unexpectedField" car il n’est pas dans le DTO.
+      forbidNonWhitelisted: true, // Lève une erreur si des propriétés non définies sont présentes, 🔥 Renvoie une erreur 400 : "unexpectedField" should not exist
       transform: true, // transforme le JSON en classe DTO
     }),
   );
